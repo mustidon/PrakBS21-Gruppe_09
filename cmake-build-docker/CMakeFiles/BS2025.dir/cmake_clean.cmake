604f269e43d24ecce437658731ae0c7cc76cb3c3
Mustafa <mustcoc17@gmail.com>
2025-05-07 17:34:17 +0200

    Projektstruktur

---
+++ cmake-build-docker/CMakeFiles/BS2025.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BS2025.dir/main.c.o"
+  "BS2025.pdb"
+  "BS2025"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BS2025.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-docker/CMakeFiles/BS2025.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BS2025.
+# This may be replaced when dependencies are built.
