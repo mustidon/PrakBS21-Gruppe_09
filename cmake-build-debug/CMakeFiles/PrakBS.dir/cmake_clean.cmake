d4fd4b084fa62fdb7d275724c1bc66aceb2c26a5
Martian0621 <mbidler@gmx.de>
2025-05-07 19:10:32 +0200

    Erster commit

---
+++ cmake-build-debug/CMakeFiles/PrakBS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PrakBS.dir/main.c.o"
+  "PrakBS.pdb"
+  "PrakBS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PrakBS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/PrakBS.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PrakBS.
+# This may be replaced when dependencies are built.
