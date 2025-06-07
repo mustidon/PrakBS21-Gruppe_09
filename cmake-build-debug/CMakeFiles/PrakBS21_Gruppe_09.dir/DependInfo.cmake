5c1f3f0924fb060a86bcf0630df3464ce972b35f
Martian0621 <mbidler@gmx.de>
2025-06-07 21:46:42 +0200

    Zweiter Meilenstein 1.0

---
+++ cmake-build-debug/CMakeFiles/PrakBS21_Gruppe_09.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/tmp/tmp.x1YV5YP0I3/KeyValueStore.c" "/tmp/tmp.x1YV5YP0I3/cmake-build-debug/CMakeFiles/PrakBS21_Gruppe_09.dir/KeyValueStore.c.o"
+  "/tmp/tmp.x1YV5YP0I3/main.c" "/tmp/tmp.x1YV5YP0I3/cmake-build-debug/CMakeFiles/PrakBS21_Gruppe_09.dir/main.c.o"
+  "/tmp/tmp.x1YV5YP0I3/sub.c" "/tmp/tmp.x1YV5YP0I3/cmake-build-debug/CMakeFiles/PrakBS21_Gruppe_09.dir/sub.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/PrakBS21_Gruppe_09.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PrakBS21_Gruppe_09.dir/main.c.o"
+  "CMakeFiles/PrakBS21_Gruppe_09.dir/KeyValueStore.c.o"
+  "CMakeFiles/PrakBS21_Gruppe_09.dir/sub.c.o"
+  "PrakBS21_Gruppe_09.pdb"
+  "PrakBS21_Gruppe_09"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PrakBS21_Gruppe_09.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
